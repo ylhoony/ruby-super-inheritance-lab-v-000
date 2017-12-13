6e9df5b72dfdfe9b96b06250aefe691aab1329c0
Hoon Lee <yl.hoony@gmail.com>
2017-12-13 08:07:56 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,4 +5,8 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let
  me just tell you who died...\n"
   end
+
+  def raise_hand
+    10.times do { super }
+  end
 end
