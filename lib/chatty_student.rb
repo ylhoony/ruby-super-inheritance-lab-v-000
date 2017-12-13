fdfe36035fbc9f82694657258e84338d4939f47e
Hoon Lee <yl.hoony@gmail.com>
2017-12-13 08:12:47 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,8 +2,7 @@ class ChattyStudent < Student
 
   def hello
     super
-    puts "How are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let
- me just tell you who died...\n"
+    puts "How are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
 
   def raise_hand
