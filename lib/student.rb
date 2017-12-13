598535db34a04029298bdd2787fb0f1afb9fdaab
Hoon Lee <yl.hoony@gmail.com>
2017-12-13 08:02:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,10 @@
 class Student
 
-  # def initialize
-  #
-  # end
-
   def hello
     puts "Hey there! I'm so excited to learn stuff.\n"
+  end
+
+  def raise_hand
+    puts "Pick me!\n"
   end
 end
